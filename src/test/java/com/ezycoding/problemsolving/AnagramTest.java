cff4234766ae0a6e67930e93e4ef107713244d91
kaushik-talukder <kaushik.talukder@gmail.com>
2020-06-11 20:35:43 -0400

    Test annotation added

--- src/test/java/com/ezycoding/problemsolving/AnagramTest.java
+++ src/test/java/com/ezycoding/problemsolving/AnagramTest.java
@@ -2,22 +2,27 @@ package com.ezycoding.problemsolving;
 
 import com.ezycoding.problemsolving.hackerrank.Anagram;
 import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AnagramTest {
 
-    private Anagram anagram;
-    @BeforeAll
-    void init(){
-        System.out.println("Before call");
-        anagram = new Anagram();
-    }
-    @Test
-    public void testAnagram(){
-        Assertions.assertTrue(anagram.checkAnagram1("abc","abc"));
-    }
-    @AfterAll
-    void cleanUp(){
-        System.out.println("Clean Up");
-    }
+	private Anagram anagram;
+
+	@BeforeAll
+	void init() {
+		System.out.println("Before call");
+		anagram = new Anagram();
+	}
+
+	@Test
+	public void testAnagram() {
+		Assertions.assertTrue(anagram.checkAnagram1("abc", "abc"));
+	}
+
+	@AfterAll
+	void cleanUp() {
+		System.out.println("Clean Up");
+	}
 }
