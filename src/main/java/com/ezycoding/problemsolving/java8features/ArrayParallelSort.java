a44ce3c2684e2d7377f1ab56eca5628e6a285377
Kaushik Talukder <kaushik.talukder@gmail.com>
2020-07-02 23:34:05 -0400

    Remove unused import

--- src/main/java/com/ezycoding/problemsolving/java8features/ArrayParallelSort.java
+++ src/main/java/com/ezycoding/problemsolving/java8features/ArrayParallelSort.java
@@ -1,9 +1,7 @@
 package com.ezycoding.problemsolving.java8features;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 
 public class ArrayParallelSort {
