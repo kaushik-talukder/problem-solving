21771db1d81fd3d4585af96f6a17f927bebbc4f1
kaushik-talukder <kaushik.talukder@gmail.com>
2020-07-09 20:59:19 -0400

    RegEx

---
+++ src/main/java/com/ezycoding/problemsolving/java8features/RegEx.java
@@ -0,0 +1,17 @@
+package com.ezycoding.problemsolving.java8features;
+
+import java.util.regex.Pattern;
+
+public class RegEx {
+    public static void main(String[] args) {
+        // Exact match
+        String input = "xy", regex = "xy";
+        System.out.println(Pattern.matches(regex, input));
+
+        input = "12345s"; regex = "\\d+\\D";
+        System.out.println(Pattern.matches(regex, input));
+
+        input = "kaushik_talukder@gmail.com"; regex = "^[a-zA-Z0-9._]+@[a-zA-Z]+.[a-zA-Z]+$";
+        System.out.println(Pattern.matches(regex, input));
+    }
+}
